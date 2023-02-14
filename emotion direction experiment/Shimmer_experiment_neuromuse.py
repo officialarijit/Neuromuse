c34c547a25c7d8fab36ecdb81ab9a86a5412413f
Arijit Nandi <nandiarijit99@gmail.com>
2023-02-14 17:57:03 +0100

    Update Shimmer_experiment_neuromuse.py

--- emotion direction experiment/Shimmer_experiment_neuromuse.py
+++ emotion direction experiment/Shimmer_experiment_neuromuse.py
@@ -19,13 +19,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import collections
 
-global PPG_ar, GSR_ar
+from paho.mqtt import client as mqtt_client
 
-data_vis_l = 300
-PPG_ar = collections.deque(np.zeros(data_vis_l))
-GSR_ar = collections.deque(np.zeros(data_vis_l))
 
-# participant_code = input("Enter participant code: ")
+
 participant_code = sys.argv[1]
 
 print('Port searching.....')
@@ -78,7 +75,7 @@ print("enable internal expansion board power, done.")
 # In[7]:
 
 
-sampling_freq = 50
+sampling_freq = 50.33
 clock_wait = int((2 << 14) / sampling_freq)
 
 ser.write(struct.pack('<BH', 0x05, clock_wait))
@@ -99,95 +96,139 @@ framesize = 8 # 1byte packet type + 3byte timestamp + 2 byte GSR + 2 byte PPG(In
 
 
 # Configure the shimmer to collect the user defined time period.
-seconds = int(10)
-t_end = time.time() + seconds
+# seconds = int(10)
+# t_end = time.time() + seconds
 
-task = 1.0 # dummy task
+# task = 1.0 # dummy task
+
+start_time = datetime.fromtimestamp(datetime.timestamp(datetime.now()))
+current_time = 0
 
 #  LSL
 # Configure a streaminfo
-info = StreamInfo(name='MyMarkerStream3', type='GSR_PPG', channel_count=3, channel_format='float32',  source_id='myuniquesourceid23443')
+# info = StreamInfo(name='Shimmer', type='GSR_PPG', channel_count=2, channel_format='float32',  source_id='myuid34234')
 # next make an outlet
-outlet = StreamOutlet(info)
-
-def read_shimmer(writer):
-    ddata = b''
-    numbytes = 0
-    framesize = 8 # 1byte packet type + 3byte timestamp + 2 byte GSR + 2 byte PPG(Int A13)
-
-    while numbytes < framesize:
-        ddata += ser.read(framesize)
-        numbytes = len(ddata)
-        
-    data = ddata[0:framesize]
-    ddata = ddata[framesize:]
-    numbytes = len(ddata)
-
-    # read packet payload
-    (PPG_raw, GSR_raw) = struct.unpack('HH', data[4:framesize])
-
-    # get current GSR range resistor value
-    Range = ((GSR_raw >> 14) & 0xff)  # upper two bits
-    if(Range == 0):
-        Rf = 40.2   # kohm
-    elif(Range == 1):
-        Rf = 287.0  # kohm
-    elif(Range == 2):
-        Rf = 1000.0 # kohm
-    elif(Range == 3):
-        Rf = 3300.0 # kohm
-    # convert GSR to kohm value
-    gsr_to_volts = (GSR_raw & 0x3fff) * (3.0/4095.0)
-    GSR_ohm = Rf/( (gsr_to_volts /0.5) - 1.0)
-
-    # convert PPG to milliVolt value
-    PPG_mv = PPG_raw * (3000.0/4095.0)
-    ts = datetime.timestamp(datetime.now())
-    mysample = [ts, GSR_raw, GSR_ohm, PPG_raw, PPG_mv]
-    # mysample = [ts, GSR_ohm, PPG_mv]
-
-    writer.writerow(mysample)
-
-    return PPG_raw, GSR_raw
-    # all_data.append(mysample)
-
-    # Send data to LSL
-    # outlet.push_sample(mysample)
-
-with open('%s_shimmer.csv'%(participant_code), 'a', newline='') as file:
-    writer = csv.writer(file)
-
-    def animation_function(i):
-        PPG_raw, GSR_raw = read_shimmer(writer)
-
-        PPG_ar.popleft()
-        GSR_ar.popleft()
-        PPG_ar.append(PPG_raw)
-        GSR_ar.append(GSR_raw)
-        ax.cla()
-        ax1.cla()
-        ax.plot(GSR_ar)
-        ax1.plot(PPG_ar)
+# outlet = StreamOutlet(info)
 
 
-    # Start animation plot
-    fig = plt.figure(figsize=(12,6), facecolor='#DEDEDE')
-    ax = plt.subplot(121)
-    ax1 = plt.subplot(122)
-    ax.set_facecolor('#DEDEDE')
-    ax1.set_facecolor('#DEDEDE')
-    ani = FuncAnimation(fig, animation_function, interval=50)
+# broker = 'broker.emqx.io'
 
-    # all_data = [] #saves the result data for the experiments
+broker = 'localhost'
+
+port = 1883
+topic = ["GSC_uS",'PPG_mv']
+client_id = 'client1'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 
-    # print("Packet Type\tTimestamp\tGSR\tPPG")
+try:
+    client = connect_mqtt()
+    client.loop_start()
+
     print("Data reccording started...")
+    with open('%s_shimmer.csv'%(participant_code), 'a', newline='') as file:
+        writer = csv.writer(file)
 
-    plt.show()
+        while True:
+            while numbytes < framesize:
+                ddata += ser.read(framesize)
+                numbytes = len(ddata)
 
-    # while True:
-    #     read_shimmer(writer)
+            data = ddata[0:framesize]
+            ddata = ddata[framesize:]
+            numbytes = len(ddata)
 
+
+            (packettype) = struct.unpack('B', data[0:1])
+            (timestamp0, timestamp1, timestamp2) = struct.unpack('BBB',data[1:4])
+
+            # read packet payload
+            (PPG_raw, GSR_raw) = struct.unpack('HH', data[4:framesize])
+
+
+
+            # get current GSR range resistor value
+            Range = ((GSR_raw >> 14) & 0xff)  # upper two bits
+            if(Range == 0):
+                Rf = 40200   # ohm
+            elif(Range == 1):
+                Rf = 287000  # ohm
+            elif(Range == 2):
+                Rf = 1000000 # ohm
+            elif(Range == 3):
+                Rf = 3300000  # ohm
+
+            # convert GSR to ohm value
+            gsr_to_volts = (GSR_raw & 0x3fff) * (3.0/4095.0)
+            GSR_ohm = Rf/( (gsr_to_volts /0.5) - 1.0)
+            GSC_uS = (1/GSR_ohm)*1000000 #uS
+            
+
+            # convert PPG to milliVolt value
+            PPG_mv = PPG_raw * (3000.0/4095.0)
+    
+            current_time = datetime.fromtimestamp(datetime.timestamp(datetime.now()))
+
+
+            ts = datetime.timestamp(datetime.now())
+            
+            delta = current_time - start_time
+            diff = delta.total_seconds()
+            
+            mysampleCSV = [ts, GSR_ohm, GSC_uS, PPG_mv, diff]            
+
+            # Send data to local MQTT
+            result1 =  client.publish(topic[0], payload =str(GSC_uS) )
+            result2 =  client.publish(topic[1], payload =str(PPG_mv) )
+
+            # result: [0, 1]
+            status1 = result1[0]
+            status2 = result2[0]
+            if status1 == 0 and status2 ==0:
+                pass
+                #print("MSG Send to topic: {} and {}".format(topic[0], topic[1]))
+            else:
+                print("Failed to send message to topics")
+
+            writer.writerow(mysampleCSV)
+
+            # print('Time diff {} sec'.format(diff))
+            
+            # if diff >=60.0:
+            #     break
+            # else:
+            #     continue
+            
+
+                
+        # all_data.append(mysample) 
+
+        # Send data to LSL
+        # outlet.push_sample(mysample)
+        
+except KeyboardInterrupt:
+#send stop streaming command
+    ser.write(struct.pack('B', 0x20))
+    print()
+    print("stop command sent, waiting for ACK_COMMAND")
+    wait_for_ack()
+    print("ACK_COMMAND received.")
+    #close serial port
+    ser.close()
+    print("All done")
 
 
